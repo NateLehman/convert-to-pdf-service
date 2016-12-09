1fda9c03721085e8b83d5466b90732c49357c76b
Nathan Lehman <njlehman1@gmail.com>
2016-12-08 20:01:21 -0600

    fixed it for curl

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 DIR=$( dirname $0 )
 
-wget -P $DIR/ https://downloadarchive.documentfoundation.org/libreoffice/old/4.3.7.2/mac/x86_64/LibreOffice_4.3.7.2_MacOS_x86-64.dmg
+curl https://downloadarchive.documentfoundation.org/libreoffice/old/4.3.7.2/mac/x86_64/LibreOffice_4.3.7.2_MacOS_x86-64.dmg > $DIR/LibreOffice_4.3.7.2_MacOS_x86-64.dmg
 
 hdiutil attach -nobrowse -mountpoint $DIR/libremount $DIR/LibreOffice_4.3.7.2_MacOS_x86-64.dmg
 cp -R $DIR/"Convert to PDF.workflow" $HOME/Library/Services/
